b41e75c58552c6f8b301a01a5624dbb1dac04fc2
sergey.domashchuk@softeq.com <sergey.domashchuk@softeq.com>
2021-06-14 00:44:33 +0300

    Navigation improved.

--- presentation/src/main/java/com/minsk/guru/screen/auth/signup/SignUpFragment.kt
+++ presentation/src/main/java/com/minsk/guru/screen/auth/signup/SignUpFragment.kt
@@ -1,17 +1,15 @@
 package com.minsk.guru.screen.auth.signup
 
-import android.app.ActivityOptions.makeSceneTransitionAnimation
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.minsk.guru.R
 import com.minsk.guru.databinding.FragmentSignUpBinding
-import com.minsk.guru.screen.home.HomeActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignUpFragment(private val layout: Int = R.layout.fragment_sign_up) : Fragment(layout) {
@@ -35,8 +33,7 @@ class SignUpFragment(private val layout: Int = R.layout.fragment_sign_up) : Frag
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         viewModel.resultLiveData.observe(viewLifecycleOwner) {
-            val intent = Intent(activity, HomeActivity::class.java)
-            startActivity(intent, makeSceneTransitionAnimation(activity).toBundle())
+            findNavController().navigate(R.id.action_signUpFragment_to_HomeFragment)
         }
         viewModel.errorLiveData.observe(viewLifecycleOwner) {
             Snackbar.make(this.requireView(), it.cause?.message.toString(), Snackbar.LENGTH_LONG)
