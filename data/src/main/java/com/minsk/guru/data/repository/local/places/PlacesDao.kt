c475c6f16c686bf0d3a5998ce542e40704b0e17c
sergey.domashchuk@softeq.com <sergey.domashchuk@softeq.com>
2021-06-22 22:39:08 +0300

    get user visited places query fixed.

--- data/src/main/java/com/minsk/guru/data/repository/local/places/PlacesDao.kt
+++ data/src/main/java/com/minsk/guru/data/repository/local/places/PlacesDao.kt
@@ -7,8 +7,8 @@ interface PlacesDao {
     @Query("SELECT * FROM places")
     fun getPlaces(): List<LocalPlace>
 
-//    @Query("SELECT * FROM places WHERE place_id IN (SELECT * FROM user_places WHERE place_id=:userId)")
-//    fun getUserVisitedPlaces(userId: String): List<LocalPlace>
+    @Query("SELECT * FROM places WHERE place_id IN (SELECT user_place_id FROM user_places WHERE user_id=:userId)")
+    fun getUserVisitedPlaces(userId: String): List<LocalPlace>
 
     @Query("SELECT * FROM places WHERE place_id=:id")
     fun getPlaceById(id: Int): LocalPlace
--- domain/src/main/java/com/minsk/guru/domain/usecase/local/places/DeleteLocalPlaceUseCaseImpl.kt
+++ domain/src/main/java/com/minsk/guru/domain/usecase/local/places/DeleteLocalPlaceUseCaseImpl.kt
@@ -1,14 +1,12 @@
 package com.minsk.guru.domain.usecase.local.places
 
 import com.minsk.guru.domain.model.UserPlace
-import com.minsk.guru.domain.repository.local.PlacesLocalRepository
 import com.minsk.guru.domain.repository.local.UserPlacesLocalRepository
 import com.minsk.guru.domain.usecase.CoroutineSingleResultUseCase
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 class DeleteLocalPlaceUseCaseImpl(
-    private val placesLocalRepository: PlacesLocalRepository,
     private val userPlacesLocalRepository: UserPlacesLocalRepository,
     ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : CoroutineSingleResultUseCase<DeleteLocalPlaceUseCase.Param, DeleteLocalPlaceUseCase.Result>(
@@ -19,7 +17,6 @@ class DeleteLocalPlaceUseCaseImpl(
     override suspend fun run(param: DeleteLocalPlaceUseCase.Param): DeleteLocalPlaceUseCase.Result =
         try {
             val place = param.place
-            placesLocalRepository.deletePlace(place) // TODO
             userPlacesLocalRepository.deleteUserPlace(UserPlace(param.userId, place.id))
             DeleteLocalPlaceUseCase.Result.Success
         } catch (error: Throwable) {
